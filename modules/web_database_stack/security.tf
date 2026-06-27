2f621dfef48fff4623893af63c6dc80b3bd47d5d
Denis Ungureanu <l3ti.me20@gmail.com>
2026-06-27 15:08:47 +0100

    Add database host extraction to GitHub Actions workflow and attach Secrets Manager policy to EC2 role

--- modules/web_database_stack/security.tf
+++ modules/web_database_stack/security.tf
@@ -87,3 +87,8 @@ resource "aws_iam_role_policy_attachment" "ssm_core" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+resource "aws_iam_role_policy_attachment" "secrets_manager_read" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
